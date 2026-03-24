b2d718f5bf8da4be756dba81e372f7aa83364cfa
developernbkc-commits <developernbkc@gmail.com>
2026-03-24 17:05:46 +0530

    Revert "RELEASE 2 2/3"
    This reverts commit 60d2052f6e3321b12b608b696318763b877b2953.

--- samvidalabs_public_website/src/lib/images.ts
+++
@@ -1,8 +0,0 @@
-import { site } from "./site";
-
-export function imgUrl(fileName: string) {
-  const base = site.imageBaseUrl || "";
-  if (!fileName) return "";
-  if (fileName.startsWith("http://") || fileName.startsWith("https://") || fileName.startsWith("/")) return fileName;
-  return base + fileName;
-}
