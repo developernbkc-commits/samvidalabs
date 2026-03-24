568600a87831699484d93db8137eeafc6a5a2900
developernbkc-commits <developernbkc@gmail.com>
2026-03-24 17:05:38 +0530

    Revert "RELEASE 2 3/3"
    This reverts commit 0a26401df72f54337091b255d74f0581169569ca.

--- samvidalabs_public_website/supabase/functions/create-registration/index.ts
+++
@@ -1,9 +0,0 @@
-// Placeholder edge function
-// Responsibilities:
-// 1. Validate learner payload
-// 2. Upsert profile
-// 3. Create registration + registration_modules rows
-// 4. Create Razorpay UPI payment link
-// 5. Return registration id, payment url, and current status
-
-export {};
